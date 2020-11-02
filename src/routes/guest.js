a171f1f1fade6afd2d42952c1d128bcb99cebb7a
ean <eanpraha@gmail.com>
2020-11-02 19:20:23 +0700

    add more crud guest

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -15,14 +15,69 @@ router.post('/', (req, res) => {
         email,
     } = req.body;
     const token = Math.floor(Math.random() * 10000) + 1;
+    const is_active = 1;
+    const sql = 'INSERT INTO guest (prefix,name,suffix,address,category_id,token,phone,email,is_active) VALUES(?,?,?,?,?,?,?,?,?)';
 
-    const sql = 'INSERT INTO guest (prefix,name,suffix,address,category_id,token,phone,email) VALUES(?,?,?,?,?,?,?,?)';
-
-    mysql.execute(sql, [prefix, name, suffix, address, category_id, token, phone, email], (err, result, field) => {
-        res.send(result);
+    mysql.execute(sql, [prefix, name, suffix, address, category_id, token, phone, email, is_active], (err, data, field) => {
+        res.send({
+            status: 200,
+            data,
+        });
         console.log(err);
     })
 })
+
+// put guest
+router.put('/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const {
+        prefix,
+        name,
+        suffix,
+        address,
+        category_id,
+        phone,
+        email,
+    } = req.body;
+    const sql = `UPDATE guest SET prefix=?, name=?, suffix=?, address=?, category_id=?, phone=?, email=? WHERE id=?`;
+    mysql.execute(sql, [prefix, name, suffix, address, category_id, phone, email, id], (err, data, field) => {
+        res.send({
+            status: 200,
+            data
+        });
+        console.log(err);
+    })
+})
+
+
+// get guest
+router.get('/', (req, res) => {
+    const sql = `select guest.id, prefix, name, suffix, address, category_id, token, phone, email, is_active, created_at, updated_at from guest order by id asc limit 0,10`
+    mysql.execute(sql, [], (err, data, field) => {
+        res.send({
+            status: 200,
+            data
+        });
+    })
+})
+
+// delete guest
+router.delete('/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const sql = 'DELETE FROM guest WHERE id=?';
+    mysql.execute(sql, [id], (err, data, field) => {
+        res.send({
+            status: 200,
+            data
+        });
+        console.log(err);
+    })
+})
+
 
 
 module.exports = router;
